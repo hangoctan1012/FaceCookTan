85b9a6327722634239fcac9b002d585c0d316940
hangoctan1012 <22521297@gm.uit.edu.vn>
2025-12-04 15:39:21 +0700

    ss

--- BackEnd_post/server.js
+++ BackEnd_post/server.js
@@ -26,7 +26,7 @@ const Comment = require("./models/commentModel");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }));
 app.use(bodyParser.json());
